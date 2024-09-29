a7b296d8a47e0e54de003dc77c11e613097e1cfe
Dhairya <118837648+Dhairya0707@users.noreply.github.com>
2024-09-29 15:43:33 +0530

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import {app} from "./app.js";
-import {connectDB} from "./db/db.js";
-
-connectDB().then(()=>
-app.listen(3000,()=>{
-    console.log("Server running on port 3000");
-})
-)
-.catch((error)=>console.log(error))
